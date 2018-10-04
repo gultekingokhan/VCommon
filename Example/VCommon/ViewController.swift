9e5fa5730d403b659ec148044ecc39557ee30cf6
Gokhan Gultekin <gokhangultekin07@hotmail.com>
2018-10-04 17:43:04 +0300

    V_ColorExtensions created.

--- Example/VCommon/ViewController.swift
+++ Example/VCommon/ViewController.swift
@@ -7,18 +7,14 @@
 //
 
 import UIKit
+import VCommon
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.backgroundColor = UIColor().hex(string: "FC5450")
     }
-
 }
 
---
+++ VCommon/Classes/V_ColorExtensions.swift
@@ -0,0 +1,33 @@
+//
+//  V_ColorExtensions.swift
+//  VCommon
+//
+//  Created by Gokhan Gultekin on 4.10.2018.
+//
+
+import Foundation
+
+public extension UIColor {
+
+    public func hex(string:String) -> UIColor {
+        var cString:String = string.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
